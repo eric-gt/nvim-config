9094a255157e3096683c741866ba8c2d0dfa25d8
Eric GT <bookjunkie93@gmail.com>
2024-03-21 13:28:26 -0400

    actually I like alpha-nvim

--- lua/egt/plugins/mini.lua
+++ lua/egt/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 			-- Startup screen
 			--
-			require("mini.starter").setup({})
+			-- require("mini.starter").setup({})
 			--
 			-- Better Around/Inside textobjects
 			--
